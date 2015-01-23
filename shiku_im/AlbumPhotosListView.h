74c29deef3c513136f5ce7ca613649c77c4d8bb9
xuliang <xuliang@xuliangdeMac.local>
2015-01-22 20:38:16 -0800

    add the new controller for show all pictures

---
+++ shiku_im/AlbumPhotosListView.h
@@ -0,0 +1,20 @@
+//
+//  AlbumPhotosListView.h
+//  SimpleCollectionViewAPI
+//
+//  Created by Simple Shi on 7/18/14.
+//  Copyright (c) 2014 Microthink Inc,. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <AssetsLibrary/ALAsset.h>
+#import <AssetsLibrary/ALAssetsGroup.h>
+#import <AssetsLibrary/ALAssetRepresentation.h>
+
+
+@interface AlbumPhotosListView : UIViewController<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic, strong) NSMutableArray *selectImages;
+@property (nonatomic, strong) UITableView *maintableview;
+@property (nonatomic, strong) ALAssetsGroup *assetGroup;
+@end
